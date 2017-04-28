e5edfcec27c997ce36f81b900e3da53432bd186b
bristanley <brian.stanley89@outlook.com>
2017-04-27 22:28:39 -0400

    added firebase to app

--- myshoppinglist/src/app/recipes/recipe.service.ts
+++ myshoppinglist/src/app/recipes/recipe.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService {
+	recipesChanged = new Subject<Recipe[]>();
 	
 	private recipes: Recipe[] = [
 	  new Recipe(
 	  	'A Test Recipe', 
 	  	'This is simply a test', 
-	  	'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+	  	'http://www.goudamonster.com/wp-content/uploads/2013/10/mediterraneanburger1.jpg',
 	  	[	  	
 	  		new Ingredient('Meat', 1),
 	  		new Ingredient('French Fries', 20)
@@ -18,7 +21,7 @@ export class RecipeService {
 	  new Recipe(
 	  	'Another Test Recipe', 
 	  	'This is simply a test', 
-	  	'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+	  	'http://i.imgur.com/mnudWZx.jpg',
 	  	[
 	  		new Ingredient('Buns', 2),
 			new Ingredient('Meat', 1),	
@@ -26,6 +29,11 @@ export class RecipeService {
 	];
 
 	constructor(private slService: ShoppingListService) {}
+
+	setRecipes(recipes: Recipe[]) {
+		this.recipes = recipes;
+		this.recipesChanged.next(this.recipes.slice());
+	}
 
 	getRecipes() {
 		return this.recipes.slice();
@@ -37,5 +45,20 @@ export class RecipeService {
 
 	addIngredientsToShoppingList(ingredients: Ingredient[]) {
 		this.slService.addIngredients(ingredients);
+	}
+
+	addRecipe(recipe: Recipe) {
+		this.recipes.push(recipe);
+		this.recipesChanged.next(this.recipes.slice());
+	}
+
+	updateRecipe(index: number, newRecipe: Recipe) {
+		this.recipes[index] = newRecipe;
+		this.recipesChanged.next(this.recipes.slice());
+	}
+
+	deleteRecipe(index: number) {
+		this.recipes.splice(index, 1);
+		this.recipesChanged.next(this.recipes.slice());
 	}
 }
