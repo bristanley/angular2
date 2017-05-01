6830b6aa6ad1f8828a187d72f6a2d513577f2ce3
bristanley <brian.stanley89@outlook.com>
2017-05-01 16:22:10 -0400

    authentication added and routing for login/login

--- myshoppinglist/src/app/app.component.ts
+++ myshoppinglist/src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   loadedFeature = 'recipe';
+
+  ngOnInit() {
+  	firebase.initializeApp({
+  		apiKey: "AIzaSyBpThE9plwt0FYapND65_SELrR9Suqj9bo",
+  		authDomain: "ng-recipe-book-ec880.firebaseapp.com",
+  	});
+  }
 
   onNavigate(feature: string) {
   	this.loadedFeature = feature;
