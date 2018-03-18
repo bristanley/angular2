7b9b1d63d0a7f9e73e094f01bb57e51d64ae7fec
bristanley <brian.stanley89@outlook.com>
2018-03-18 16:45:32 -0400

    update ng and ng-cli

--- myshoppinglist/src/app/app.module.ts
+++ myshoppinglist/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -8,6 +9,7 @@ import { SharedModule } from './shared/share.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { CoreModule } from './core/core.module';
 import { AuthModule } from './auth/auth.module';
+
  
 @NgModule({
   declarations: [
